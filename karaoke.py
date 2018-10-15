3a8551c89c9466988372772c82ae9d29e559991c
sergiomc95 <sergiomartinc95@gmail.com>
2018-10-15 19:46:50 +0200

    termino ej4

--- karaoke.py
+++ karaoke.py
@@ -4,31 +4,28 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 import sys
+import json
 from smallsmilhandler import SmallSMILHandler
 
 if __name__ == "__main__":
 
-
     parser = make_parser()
     cHandler = SmallSMILHandler()
-
     parser.setContentHandler(cHandler)
-    fichero = sys.argv[1]
     try:
-        fichero = sys.argv[1]
+	       fichero = sys.argv[1]
     except IndexError:
-        sys.exit("Usage: python3 karaoke.py file.smil")
+		   sys.exit("Usage: python3 karaoke.py file.smil")
+
     parser.parse(open(fichero))
-
     misdatos = cHandler.get_tags()
-
-    for etiqueta in misdatos:
-        salida = ""
-        elemento = etiqueta.items() #es un diccionario
+    salida = ""
     for datos in misdatos:
-    	print(elemento)
-    	for etiqueta in datos:
-    		for atributo, valor in datos[etiqueta].items():
-    			salida = salida + etiqueta + " \ " + atributo + '="' + valor + '"\ '
-    	print(salida)
-    	salida = ""
+        for etiqueta in datos:
+
+                for atributo, valor in datos[etiqueta].items():
+
+                    salida = salida + '\t' + atributo + ' = "' + valor + '"\t'
+        print(etiqueta + salida)
+        salida = ""
+    json.dump(misdatos, open('karaoke.json', 'w'))
