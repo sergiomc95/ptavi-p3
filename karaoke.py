684e11cd5e1fec6666d41f587f59b67427e0b56a
sergiomc95 <sergiomartinc95@gmail.com>
2018-10-16 18:12:42 +0200

    ejercicio 7 incompleto

--- karaoke.py
+++ karaoke.py
@@ -5,32 +5,50 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 import sys
 import json
-from smallsmilhandler import SmallSMILHandler
 import urllib.request
+from smallsmilhandler import SmallSMILHandler
+
+class KaraokeLocal():
+    def Inicializador(self, fichero):
+        self.cHandler = SmallSMILHandler()
+        self.misdatos = []
+        self.fich = fichero
+
+        parser = make_parser()
+        parser.setContentHandler(self.cHandler)
+        parser.parse(open(self.fich))
+        misdatos = self.cHandler.get_tags()
+
+        return misdatos
+
+    def toStr(self, datos):
+        salida = ""
+
+        for elementos in datos:
+            for etiqueta in elementos:
+                for atributo, valor in elementos[etiqueta].items():
+                    if (atributo == 'src') and (valor[0:7] == 'http://'):
+                        valorNuevo = valor.split("/")[-1]
+                        urllib.request.urlretrieve(valor, valorNuevo)
+                        salida = salida + '\t' + atributo + '="' + valorNuevo + '"\t'
+                    else:
+                        salida = salida + '\t' + atributo + ' = "' + valor + '"\t'
+            print(etiqueta + salida)
+            salida = ""
+
+    def to_json(self, datos):
+        json.dump(datos, open('karaoke.json', 'w'))
+
 if __name__ == "__main__":
 
-    parser = make_parser()
-    cHandler = SmallSMILHandler()
-    parser.setContentHandler(cHandler)
+    objKaraokeLocal = KaraokeLocal()
+    datos = []
+
     try:
-	       fichero = sys.argv[1]
+        fichero = sys.argv[1]
     except IndexError:
-		   sys.exit("Usage: python3 karaoke.py file.smil")
+        sys.exit("Usage: python3 karaoke.py file.smil")
 
-    parser.parse(open(fichero))
-    misdatos = cHandler.get_tags()
-    salida = ""
-    for datos in misdatos:
-        for etiqueta in datos:
-                    for atributo, valor in datos[etiqueta].items():
-                        if (atributo == 'src') and (valor[0:7] == 'http://'):
-                            valorNuevo = valor.split("/")[-1]
-                            urllib.request.urlretrieve(valor, valorNuevo)
-                            salida = salida + '\t' + atributo + '="' + valorNuevo + '"\t'
-
-                        else:
-
-                            salida = salida + '\t' + atributo + ' = "' + valor + '"\t'
-        print(etiqueta + salida)
-        salida = ""
-    json.dump(misdatos, open('karaoke.json', 'w'))
+    datos = objKaraokeLocal.Inicializador(fichero)
+    objKaraokeLocal.toStr(datos)
+objKaraokeLocal.to_json(datos)
